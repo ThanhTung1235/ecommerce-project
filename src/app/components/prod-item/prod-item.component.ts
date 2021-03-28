61d42cce173d333cd89af80a0cf9f5350ded7554
NDTRONG <tron09796@gmail.com>
2021-03-28 10:42:35 +0700

    push dataa

--- src/app/components/prod-item/prod-item.component.ts
+++ src/app/components/prod-item/prod-item.component.ts
@@ -1,17 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-prod-item',
   templateUrl: './prod-item.component.html',
   styleUrls: ['./prod-item.component.scss']
 })
-export class ProdItemComponent implements OnInit {
+export class ProdItemComponent implements OnInit, OnChanges {
   @Input() searchResult: boolean;
   currentRate = 6;
   data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  listProduct = [];
+  totalProduct = 0;
+  loading = false;
 
-  constructor(config: NgbRatingConfig) {
+  constructor(
+    config: NgbRatingConfig,
+    private productService: ProductService
+  ) {
     config.max = 5;
     config.readonly = true;
   }
@@ -19,4 +26,13 @@ export class ProdItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.loading = true;
+    this.productService.getListProduct({}).subscribe(response => {
+      if (response.status_code === 200) {
+        this.listProduct = response.data.result;
+        this.totalProduct = response.data.total;
+      }
+    });
+  }
 }
