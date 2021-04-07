8e3156c7ba7d785c2cb1a7653c6253fca5d4a7f2
Le Thanh Tung <thanhtungle1235@gmail.com>
2021-04-07 08:08:38 +0700

    notify card in desktop

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,24 +1,34 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class BaseService {
+  private subject = new Subject();
 
-constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
-get requestHeaders(): any {
-  return this.createAPIHeader();
-}
+  sendData(data: any){
+    this.subject.next(data);
+  }
 
-private createAPIHeader(): any {
-  return new HttpHeaders({
-    'Content-Type': 'application/json'
-  });
-}
+  clearData(data: any){
+    this.subject.next();
+  }
 
+  getData(): Observable<any>{
+    return this.subject.asObservable();
+  }
 
+  get requestHeaders(): any {
+    return this.createAPIHeader();
+  }
 
+  private createAPIHeader(): any {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+  }
 }
