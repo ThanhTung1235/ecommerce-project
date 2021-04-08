da6e013e66e1cf95721c20a9310c306cfa8629c4
Le Thanh Tung <thanhtungle1235@gmail.com>
2021-04-08 08:27:06 +0700

    sua ten file

--- src/app/containers/shopping-cart/shopping-cart.module.ts
+++ src/app/containers/shopping-cart/shopping-cart.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ShoppingCartComponent } from './shopping-cart.component';
 
 import { Routes, RouterModule } from '@angular/router';
-import { SharedModule } from 'src/app/shared/shared,module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {path: '', component : ShoppingCartComponent}
