823ac89fe127ddcd03e9fb2815a78afc558bf21a
NDTRONG <tron09796@gmail.com>
2021-06-03 21:45:15 +0700

    FIX ERROR BUILD

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,5 +1,5 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from './HttpErrorInterceptor';
+import { HttpErrorInterceptor} from './httpErrorInterceptor';
 
 export const httpInterceptorProviders = [
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
