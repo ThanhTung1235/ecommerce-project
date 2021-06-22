81c367491a7a397c5ce154fb8913933862954ef6
NDTRONG <tron09796@gmail.com>
2021-06-23 00:50:09 +0700

    code

--- src/app/components/auth/address/address/address.component.ts
+++ src/app/components/auth/address/address/address.component.ts
@@ -1,8 +1,8 @@
-import { AppUtils } from 'src/app/utils/app.utils';
-import { AddressService } from '../../../../services/address.service';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { CustomerAddress } from 'src/app/models/address';
+import {AppUtils} from 'src/app/utils/app.utils';
+import {AddressService} from '../../../../services/address.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {CustomerAddress} from 'src/app/models/address';
 
 @Component({
   selector: 'app-address',
@@ -14,7 +14,7 @@ export class AddressComponent implements OnInit {
   list_cities_fetch = [];
   list_district_fetch = [];
   list_ward_fetch = [];
-  list_user_address= [];
+  list_user_address = [];
   customer_address = new CustomerAddress();
   isLoading = false;
   customer_info: any;
@@ -28,17 +28,18 @@ export class AddressComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private addressService: AddressService) { }
+    private addressService: AddressService) {
+  }
 
   ngOnInit() {
-    this.customer_address.city_id = "0";
-    this.customer_address.district_id = "0";
-    this.customer_address.ward_id = "0";
+    this.customer_address.city_id = '0';
+    this.customer_address.district_id = '0';
+    this.customer_address.ward_id = '0';
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
       }
-      const url = evt.url
+      const url = evt.url;
       this.getTitlePage(url);
     });
     this.getTitlePage(this.router.url);
@@ -47,7 +48,7 @@ export class AddressComponent implements OnInit {
     this.detectUrl();
   }
 
-  detectUrl(): void{
+  detectUrl(): void {
     // this.userAddressId = this.route.snapshot.paramMap.get('edit');
     // this.getUserAddressDetail(this.userAddressId);
     this.route.queryParams.subscribe(params => {
@@ -56,25 +57,25 @@ export class AddressComponent implements OnInit {
   }
 
   getUserInfoFromJwt() {
-    let token = AppUtils.getDataFromCookies('re_tk')
-    this.customer_info =  AppUtils.parseJwt(token)['identity'];
+    let token = AppUtils.getDataFromCookies('re_tk');
+    this.customer_info = AppUtils.parseJwt(token)['identity'];
   }
 
   editAddress(event) {
     if (event) {
-      this.router.navigate([], {relativeTo : this.route, queryParams: {edit: event}});
+      this.router.navigate([], {relativeTo: this.route, queryParams: {edit: event}});
     } else {
-      this.router.navigate([], {relativeTo : this.route, queryParams: {edit: event}});
+      this.router.navigate([], {relativeTo: this.route, queryParams: {edit: event}});
     }
-    this.show_address_form = true
+    this.show_address_form = true;
   }
 
 
-  getTitlePage(url){
+  getTitlePage(url) {
     if (url.includes('tai-khoan/dia-chi')) {
       if (url.includes('/dia-chi?edit=')) {
         this.show_address_form = true;
-      }else {
+      } else {
         this.show_address_form = false;
       }
     }
@@ -82,28 +83,39 @@ export class AddressComponent implements OnInit {
 
   getListUserAddress() {
     this.addressService.getUserAddress(this.limit, this.page).subscribe(res => {
-      if (res.status_code == 200) {
+      if (res.status_code === 200) {
         this.list_user_address = res.data.result;
-        this.totalUserAddress = res.data.total
+        this.totalUserAddress = res.data.total;
         this.list_user_address = this.list_user_address.sort((a, b) => {
-          if (b.status < a.status) return -1;
-          if (b.status > a.status) return 1;
+          if (b.status < a.status) {
+            return -1;
+          }
+          if (b.status > a.status) {
+            return 1;
+          }
           return 0;
         });
       }
-    })
+    });
   }
 
   listUserAddressChange(data) {
     if (data == true) {
       this.getListUserAddress();
     }
-    
+
   }
 
-  pageChanging(event){
+  pageChanging(event) {
     this.page = event;
     this.getListUserAddress();
   }
 
+  deleteAddress(uid) {
+    this.addressService.deleteAddress(uid).subscribe(res => {
+      if (res.status_code === 200) {
+        this.getListUserAddress();
+      }
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://171.244.8.167:23168/u1',
-  endpoint_address: 'http://171.244.8.167:23168/v1',
-  auth_endpoint: 'http://171.244.8.167:23168/user'
+  endpoint: 'http://206.189.86.199:23168/u1',
+  endpoint_address: 'http://206.189.86.199:23168/v1',
+  auth_endpoint: 'http://206.189.86.199:23168/user'
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
+ * For easier debugging in development mode, you can import the following fileSổ địa chỉ
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
